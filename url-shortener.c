1fd63baf19675babf941c78540a971eb90318697
Marko Sanchez <sanchezmarco0909@gmail.com>
2020-08-01 21:43:10 -0700

    Added Url hasher

--- url-shortener.c
+++ url-shortener.c
@@ -3,36 +3,49 @@
 
 #include <mongoc/mongoc.h>
 
-//char* b62_converter(char* url);
-bool insert_DB( mongoc_client_t* ,mongoc_collection_t*, mongoc_database_t* );
-bool query_DB(mongoc_client_t*);
 
-int main(){
+void b62_converter(char* , char [] );
+bool insert_DB( mongoc_client_t*, char*, char*);
+bool query_DB(mongoc_client_t*, char*);
+
+int main(int argc, char* argv[]){
 	
 	// Variables:
 	mongoc_client_t *client;
-	mongoc_database_t *database;
-	mongoc_collection_t *collection;
+	char* url;
+
+	// Get URl:
+	if( argc > 0){
+		url = (char*) argv[--argc];
+	}else{
+		return EXIT_FAILURE;
+	}
 
 	// INIT: libmongoc's internals.
 	mongoc_init();
 
 	// Create new client instance:
 
-	// Insertion:
-	if(false)
-	if( !insert_DB(client, collection, database) ){
-		fprintf(stderr, "Failed Insertion\n");
+	// Hash URL:
+	char hashed_url[27];
+	if(false){
+	b62_converter(url,hashed_url);
+	printf("URL: %s\nHash: %s\n",url,hashed_url);
 	}
 
+	// Insertion:
+	if(false)
+	if( !insert_DB(client, hashed_url, url) ){
+		fprintf(stderr, "Failed Insertion\n");
+	}
+	
 	// Query:
-	if( ! query_DB(client) ){
+	if(true)
+	if( ! query_DB(client,hashed_url) ){
 		fprintf(stderr, "Query Failed\n");
 	}
 
 	// Clean up libmongoc.
-	mongoc_collection_destroy (collection);
-	mongoc_database_destroy (database);
 	mongoc_client_destroy (client);
 	mongoc_cleanup ();
 
@@ -40,21 +53,41 @@ int main(){
 	
 }
 
-bool query_DB(mongoc_client_t *client){
+void  b62_converter( char* url, char m_url[] ){
+
+	unsigned int url_length = strlen(url); 
+	unsigned int base = 62;
+
+	char compressor[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	long url_num = 0;
+
+	for(unsigned int i = 0; i < url_length; ++i){
+		url_num += (int) url[i];
+	}
+
+	int counter = 0;
+	while( url_num > 0){
+		m_url[counter] = compressor[ url_num % base ]; 
+		
+		url_num /= base;
+		counter++;
+	}
+	m_url[counter] = '\0';
+
+}
+
+bool query_DB(mongoc_client_t *client, char* key){
 
 	// Variables:
-	mongoc_collection_t *collection;
 	mongoc_cursor_t *cursor;
 	const bson_t *doc;
 	bson_t *query;
 	char *str;
 	
-	collection = mongoc_client_get_collection( client, "URL_lib","coll_name");
+	mongoc_collection_t *collection = mongoc_client_get_collection( client, "URL_lib","coll_name");
 	
 	query = bson_new();
-	//query = BCON_NEW("1adfgs",BCON_TYPE_REGEX("."));
-	bson_append_regex(query, "1adfgs",-1, ".", NULL);
-	//BSON_APPEND_UTF8(query, "1adfgs", BCON_TYPE_REGEX(".") );
+	bson_append_regex(query, key,-1, ".", NULL);
 	cursor = mongoc_collection_find_with_opts( collection, query, NULL, NULL);
 
 	while( mongoc_cursor_next( cursor, &doc) ){
@@ -63,22 +96,25 @@ bool query_DB(mongoc_client_t *client){
 		bson_free(str);
 	}
 
-	bson_destroy(query);
+	// Clean libmongoc:
+	mongoc_collection_destroy(collection);
 	mongoc_cursor_destroy(cursor);
+
+	bson_destroy(query);
 	
 	return true;
 }
 
 
-bool insert_DB(mongoc_client_t *client,mongoc_collection_t *collection, mongoc_database_t *database){
+bool insert_DB(mongoc_client_t *client, char* key, char* value){
 	
 	// Variables:	
 	bson_t *command, reply, *insert;
 	bson_error_t error;
 	
-	// Get Database & Ccollection
-	database = mongoc_client_get_database( client, "URL_lib");
-	collection = mongoc_client_get_collection( client, "URL_lib","coll_name");
+	// Get Database & Collection:
+	mongoc_database_t *database = mongoc_client_get_database( client, "URL_lib");
+	mongoc_collection_t *collection = mongoc_client_get_collection( client, "URL_lib","coll_name");
 
 	command = BCON_NEW("ping", BCON_INT32(1));
 
@@ -93,11 +129,15 @@ bool insert_DB(mongoc_client_t *client,mongoc_collection_t *collection, mongoc_d
 	char *str = bson_as_json(&reply, NULL);
 	printf("%s\n", str);
 
-	insert = BCON_NEW("1adfgs",BCON_UTF8("www.youtube.com/"));// Key: value
+	insert = BCON_NEW(key , BCON_UTF8(value) );// Key: value
 
 	if(!mongoc_collection_insert(collection,MONGOC_INSERT_NONE,insert,NULL, &error)){
 		fprintf( stderr, "%s\n",error.message);
 	}
+
+	// Clean libmongc:
+	mongoc_collection_destroy (collection);
+	mongoc_database_destroy (database);
 
 	bson_destroy (insert);
 	bson_destroy (&reply);
@@ -108,24 +148,3 @@ bool insert_DB(mongoc_client_t *client,mongoc_collection_t *collection, mongoc_d
 
 }
 
-/*
-char*  b62_converter( char* url ){
-
-	char compressor[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char* compressed_str;
-	long url_num;
-	int url_length = url.length(); 
-	int base = 62;
-
-	for(unsigned int i = 0; i < url_length; ++i){
-		url_num += (int) url[i];
-	}
-
-	while( url_num > 0){
-		compressed_str = compressor[url_num % base ] + compressed_str;
-		url_num /= base;
-	}
-
-	return  compressed_str;
-}
-*/
