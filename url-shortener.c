bed829d89471f1ee3b8aa50da4d2031f76bbd0eb
Marko Sanchez <sanchezmarco0909@gmail.com>
2020-08-07 20:56:26 -0700

    Added threads / testing threads

--- url-shortener.c
+++ url-shortener.c
@@ -1,16 +1,30 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "functions.h"
 
-// Holds user input.
+// Holds user input. Might be Deprecated due to thrad creation.
 struct ARGS{
-	uint8_t threads;
+	int threads;
 	char* url;
 	char hashed_url[27];
 	mongoc_client_t *client;
 };
+
+struct thread_arg_t{
+	int thread_id;
+};
+
+// Global varables.
+
+/* Muxes */
+pthread_mutex_t request_mutex;
+pthread_mutexattr_t mta;
+
+/* Condition Variable */
+pthread_cond_t got_request = PTHREAD_COND_INITIALIZER;
 
 /**
  * Parse user terminal input; options: amount of threads, insert url and queue database.
@@ -26,7 +40,7 @@ void arg_parser(int argc, char* argv[],struct ARGS *args){
 		switch(opt){
 			case 'i':
 				printf("Number of Threads: %d\n",atoi(optarg));
-				args->threads = opt;
+				args->threads = atoi(optarg);
 				break;
 			case 'u':
 				printf("URL to be hashed: %s\n",optarg);
@@ -48,6 +62,40 @@ void arg_parser(int argc, char* argv[],struct ARGS *args){
 		printf("Extra Arguments: %s\n", argv[optind]);
 
 }
+/**
+ *  Handles request given to thread.
+ *  @params { struct thread_arg_t } holds information about the thread
+ */
+void *handle_request(void *thread_arg){
+	
+	pthread_mutex_lock(&request_mutex);
+	struct thread_arg_t args = *(struct thread_arg_t*) thread_arg;
+	uint8_t id = args.thread_id;
+	printf( "Thread %d online...\n",id);
+	pthread_mutex_unlock(&request_mutex);
+
+	sleep(1);
+	/*
+	// Url hash converter:
+	if(false){
+	b62_converter(arguements.url,arguements.hashed_url);
+	printf("URL: %s\nHash: %s\n",arguements.url,arguements.hashed_url);
+	}
+
+	// Insertion:
+	if(false)
+	if( !insert_DB(arguements.client, arguements.hashed_url, arguements.url) ){
+		fprintf(stderr, "Failed Insertion\n");
+	}
+	
+	// Query:
+	if(false)
+	if( ! query_DB(arguements.client,arguements.hashed_url) ){
+		fprintf(stderr, "Query Failed\n");
+	}
+	*/
+
+}
 int main(int argc, char* argv[]){
 	
 	// Variables:
@@ -60,24 +108,26 @@ int main(int argc, char* argv[]){
 
 	// Create new client instance:
 
-	// Url hash converter:
-	if(true){
-	b62_converter(arguements.url,arguements.hashed_url);
-	printf("URL: %s\nHash: %s\n",arguements.url,arguements.hashed_url);
+	// Init: Threads & Mutxes.
+	pthread_mutexattr_init(&mta);
+	pthread_mutexattr_settype(&mta, PTHREAD_MUTEX_RECURSIVE);
+	pthread_mutex_init(&request_mutex, &mta);
+
+	pthread_t threads [ arguements.threads ];
+	struct thread_arg_t thread_args[ arguements.threads ];
+
+	// Create user inputed amount of threads.
+	for( int i = 0; i < arguements.threads; i++){
+		// Intialize arguements for thread.
+		thread_args[i].thread_id = i;
+		if(pthread_create( &threads[i], NULL, handle_request,&thread_args[i]) ){
+			fprintf(stderr, "Error creating Thread\n");
+		}
 	}
 
-	// Insertion:
-	if(true)
-	if( !insert_DB(arguements.client, arguements.hashed_url, arguements.url) ){
-		fprintf(stderr, "Failed Insertion\n");
-	}
+	/* Server: TODO Getting ready to set up*/
+	sleep(5);
 	
-	// Query:
-	if(true)
-	if( ! query_DB(arguements.client,arguements.hashed_url) ){
-		fprintf(stderr, "Query Failed\n");
-	}
-
 	// Clean up libmongoc.
 	mongoc_client_destroy (arguements.client);
 	mongoc_cleanup ();
